a6fe846b35e65cbcbaf3e7f3db0c96d58b6cf1c8
Oluwadamilare <oluwadamilare.alonge@gmail.com>
2024-01-04 17:20:24 +0100

    Modified 👌

--- js/jquery.ajaxchimp.js
+++ js/jquery.ajaxchimp.js
@@ -1,147 +1,54 @@
-/*!
-Mailchimp Ajax Submit
-jQuery Plugin
-Author: Siddharth Doshi
-
-Use:
-===
-$('#form_id').ajaxchimp(options);
-
-- Form should have one <input> element with attribute 'type=email'
-- Form should have one label element with attribute 'for=email_input_id' (used to display error/success message)
-- All options are optional.
-
-Options:
-=======
-options = {
-    language: 'en',
-    callback: callbackFunction,
-    url: 'http://blahblah.us1.list-manage.com/subscribe/post?u=5afsdhfuhdsiufdba6f8802&id=4djhfdsh99f'
-}
-
-Notes:
-=====
-To get the mailchimp JSONP url (undocumented), change 'post?' to 'post-json?' and add '&c=?' to the end.
-For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsiufdba6f8802&id=4djhfdsh99f&c=?',
-*/
-
-(function($) {
-    'use strict';
-
-    $.ajaxChimp = {
-        responses: {
-            'We have sent you a confirmation email': 0,
-            'Please enter a value': 1,
-            'An email address must contain a single @': 2,
-            'The domain portion of the email address is invalid (the portion after the @: )': 3,
-            'The username portion of the email address is invalid (the portion before the @: )': 4,
-            'This email address looks fake or invalid. Please enter a real email address': 5
-        },
-        translations: {
-            'en': null
-        },
-        init: function(selector, options) {
-            $(selector).ajaxChimp(options);
-        }
-    };
-
-    $.fn.ajaxChimp = function(options) {
-        $(this).each(function(i, elem) {
-            var form = $(elem);
-            var email = form.find('input[type=email]');
-            var label = form.find('label[for=' + email.attr('id') + ']');
-
-            var settings = $.extend({
-                'url': form.attr('action'),
-                'language': 'en'
-            }, options);
-
-            var url = settings.url.replace('/post?', '/post-json?').concat('&c=?');
-
-            form.attr('novalidate', 'true');
-            email.attr('name', 'EMAIL');
-
-            form.submit(function() {
-                var msg;
-
-                function successCallback(resp) {
-                    if (resp.result === 'success') {
-                        msg = 'We have sent you a confirmation email';
-                        label.removeClass('error').addClass('valid');
-                        email.removeClass('error').addClass('valid');
-                    } else {
-                        email.removeClass('valid').addClass('error');
-                        label.removeClass('valid').addClass('error');
-                        var index = -1;
-                        try {
-                            var parts = resp.msg.split(' - ', 2);
-                            if (parts[1] === undefined) {
-                                msg = resp.msg;
-                            } else {
-                                var i = parseInt(parts[0], 10);
-                                if (i.toString() === parts[0]) {
-                                    index = parts[0];
-                                    msg = parts[1];
-                                } else {
-                                    index = -1;
-                                    msg = resp.msg;
-                                }
-                            }
-                        } catch (e) {
-                            index = -1;
-                            msg = resp.msg;
-                        }
-                    }
-
-                    // Translate and display message
-                    if (
-                        settings.language !== 'en' &&
-                        $.ajaxChimp.responses[msg] !== undefined &&
-                        $.ajaxChimp.translations &&
-                        $.ajaxChimp.translations[settings.language] &&
-                        $.ajaxChimp.translations[settings.language][$.ajaxChimp.responses[msg]]
-                    ) {
-                        msg = $.ajaxChimp.translations[settings.language][$.ajaxChimp.responses[msg]];
-                    }
-                    label.html(msg);
-
-                    label.show(2000);
-                    if (settings.callback) {
-                        settings.callback(resp);
-                    }
-                }
-
-                var data = {};
-                var dataArray = form.serializeArray();
-                $.each(dataArray, function(index, item) {
-                    data[item.name] = item.value;
-                });
-
-                $.ajax({
-                    url: url,
-                    data: data,
-                    success: successCallback,
-                    dataType: 'jsonp',
-                    error: function(resp, text) {
-                        console.log('mailchimp ajax submit error: ' + text);
-                    }
-                });
-
-                // Translate and display submit message
-                var submitMsg = 'Submitting...';
-                if (
-                    settings.language !== 'en' &&
-                    $.ajaxChimp.translations &&
-                    $.ajaxChimp.translations[settings.language] &&
-                    $.ajaxChimp.translations[settings.language]['submit']
-                ) {
-                    submitMsg = $.ajaxChimp.translations[settings.language]['submit'];
-                }
-                label.html(submitMsg).show(2000);
-
-                return false;
-            });
+// Function to handle form submission and display modal
+function handleSubmit(form, scriptURL, modalID) {
+    const modal = document.getElementById(modalID);
+    const closeBtn = modal.querySelector('.close');
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+  
+      const btnSubmit = form.querySelector('.btn-submit');
+      btnSubmit.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...';
+      btnSubmit.disabled = true;
+  
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+          if (response.ok) {
+            modal.style.display = 'block';
+            setTimeout(() => {
+              modal.style.display = 'none';
+              window.location.reload();
+            }, 2000);
+          } else {
+            throw new Error('Network response was not ok.');
+          }
+        })
+        .catch(error => {
+          console.error('Error!', error.message);
+          btnSubmit.innerHTML = 'Join Waitlist <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512", fill="currentColor"><path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>';
+          btnSubmit.disabled = false;
         });
-        return this;
+    });
+  
+    // Close the modal when clicking on the close button
+    closeBtn.onclick = function () {
+      modal.style.display = 'none';
     };
-})(jQuery);
+  
+    // Close the modal when clicking outside the modal content
+    window.onclick = function (event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    };
+  }
+  
+  // Usage for the waitlist form
+  const waitlistForm = document.forms['waitlistform'];
+  const waitlistScriptURL = 'https://script.google.com/macros/s/AKfycbzyuQOn1cv-qOiRjqH8xNN22G9CU5JP8zCVbykBPCmwx7P_TpLt1tG0IvqcV7VGQfc/exec';
+  handleSubmit(waitlistForm, waitlistScriptURL, 'waitlistModal');
+  
+  // Usage for the contact form
+  const contactForm = document.forms['contactform'];
+  const contactScriptURL = 'https://script.google.com/macros/s/AKfycbykmXjU3AzpyB3AxR6AZf6qaWYVAaHOPaA98IVWA0mIwxFIz3rE9tJxOyxAnp4jDdewZg/exec';
+  handleSubmit(contactForm, contactScriptURL, 'contactModal');
+  
